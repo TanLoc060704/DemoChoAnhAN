2a0c7ebdf88f68510edc9a472998d7efa59eb2c2
TanLoc060704 <128579533+TanLoc060704@users.noreply.github.com>
2023-12-08 13:06:42 +0700

    create file

---
+++ AnhAN/src/main/java/anhan/helll0.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package anhan;
+
+/**
+ *
+ * @author TAN LOC
+ */
+public class helll0 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
