06f66fbf6633798bc7de43e83cf2835cc6a87fd5
TanLoc060704 <128579533+TanLoc060704@users.noreply.github.com>
2023-12-08 11:19:11 +0700

    Create

---
+++ AnhAN/src/main/java/com/edusys/anhan/AnhAN.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.edusys.anhan;
+
+/**
+ *
+ * @author TAN LOC
+ */
+public class AnhAN {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
